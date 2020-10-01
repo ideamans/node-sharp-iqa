78d0fa4e63ef3fbe6ea8ad00a2d4e207f73bf7ca
Kunihiko Miyanaga <miyanaga@ideamans.com>
2020-10-01 16:52:13 +0900

    Upgrade sharp to 0.26.x

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import Sharp from 'sharp';
-export declare function mse(img0: Sharp.Sharp, img1: Sharp.Sharp): Promise<number>;
-export declare function psnr(img0: Sharp.Sharp, img1: Sharp.Sharp): Promise<number>;
